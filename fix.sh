e85deffa06edc533b0ba3985ce2c33e38e966036
Alkhanet <92195275+alkhanet26@users.noreply.github.com>
2023-05-26 21:08:08 +0700

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 echo -e "Download binaries"
-wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/fix-tinyfm/main/oceditor.php" -O /www/tinyfm/oceditor.php && chmod 755 /www/tinyfm/oceditor.php
-wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/fix-tinyfm/main/tinyfm.php" -O /www/tinyfm/tinyfm.php && chmod 755 /www/tinyfm/tinyfm.php
-wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/fix-tinyfm/main/tinyfm_online.php" -O /www/tinyfm/tinyfm_online.php && chmod 755 /www/tinyfm/tinyfm_online.php
+wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/fix-tinyfm/main/oceditor.php" -O /www/oceditor.php && chmod 755 /www/oceditor.php
+wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/fix-tinyfm/main/oceditor.htm" -O /usr/lib/lua/luci/view/openclash/oceditor.htm && chmod 755 /usr/lib/lua/luci/view/openclash/oceditor.htm
 clear
 echo -e "Fix TinyFM Finished"
 sleep 3
